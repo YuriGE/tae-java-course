5c53521383c15e592e291c7203c3f287c0355068
YuriGE <kirkitadze95@gmail.com>
2026-05-31 05:45:51 +0400

    [Yuri K.] add Prime Check

---
+++ src/main/java/homework4/PrimeCheck.java
@@ -0,0 +1,26 @@
+package homework4;
+
+public class PrimeCheck {
+    public static void main(String[] args) {
+        int age = 31;
+        boolean isPrime = true;
+
+        if (age <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= age / 2; i++) {
+                if (age % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println("ასაკი = " + age + ", ეს არის მარტივი რიცხვი.");
+        } else {
+            System.out.println("ასაკი = " + age + ", ეს არ არის მარტივი რიცხვი.");
+        }
+    }
+}
+
